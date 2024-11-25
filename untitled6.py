ad42bbbf51b7219bba8481d56c8c595962be925f
servicio360 <servicio360@outlook.es>
2024-11-25 11:14:51 -0500

    secccion18

---
+++ untitled6.py
@@ -0,0 +1,15 @@
+#condiciones simples
+tem=int(input("INGRESE LA TEMPERATURA"))
+#condicion simples
+if tem > 20:
+    print("EL AIRE ESTA APAGADO")
+else: # condicional
+    print("EL AIRE ESTA BIEN")
+numero=int(input("INGRESE UN NUMERO"))
+
+if(numero>0):
+    print("EL NUMERO ES POSITIVO")
+elif(numero<0):
+    print("EL NUMERO ES NEGATIVO")
+else:
+    print("EL NUMERO ES NEUTRO")
---
+++ untitled7.py
@@ -0,0 +1,13 @@
+# cilco for 
+for x in range(10): # inicia en 0 va hasta el 10
+    print(x)
+print("aqui cambio")
+for x in range(12,25): #inicia en 12 va hasta el 25
+    print(x)
+print("aqui va el decremento")
+
+for x in range(55, 25, -3):#inicia en 55 y va hasta el 25 de 3 en 3 desenso
+    print(x)
+    
+cantidad=10
+menor=0
